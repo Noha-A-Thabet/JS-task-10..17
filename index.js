33bc8f5f8658d6b6868f3a3bc62648e68f711ffc
Noha-A-Thabet <NohaAshraf25@outlook.com>
2022-05-06 13:45:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// task 1 
+let numberOne = 10;
+let numberTwo = 20;
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne} ${numberTwo}`);
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo));
+console.log(numberOne + "\n" +
+    numberTwo);
+console.log(`${numberOne}
+${numberTwo}`)
+
+
+
+// task 3 
+let one = "i'm in ";
+let two = "Love";
+let three = "with";
+let four = "javascript";
+
+console.log("\`" + one + "\n" + "\\\\" + two + "\\\\" + "\"\"\"" + ' \'\'\'' + "\n" + "++ " + three + " ++" + "\\" + "\"\"\"" + " \\" + "\"\"\"" + "\n" + "\"\"" + four + "\"\"" + "\`\`")
+
+
+// task 4 
+let a = 21;
+let b = 20;
+
+console.log("\/\/" + "\_" + a + "\_" + (b + "" + a) + "\_" + (b + "" + a) + "\_" + (b + "" + a) + "\_" + b)
